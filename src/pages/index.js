780f9aedc5ef59d078d362cb474f9ef25c2bd8f8
Aksel Nordvig Ladegaard <an@acto.dk>
2022-07-21 19:06:29 +0200

    Auto stash before checking out "origin/main"

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,16 @@ const IndexPage = () => {
 		<Layout>
 			<Section>
 				<h1>This is the front page</h1>
+				<p>There is not a lot to find here... YET!</p>
+				<p>
+					There is some barebones content under experiments and a few
+					technical details on the about page
+				</p>
+				<p>Still under active development as of 2022-07-21</p>
+				<p>
+					Its' just a little slow, cause I get distracted by pointless
+					technicalities.{' '}
+				</p>
 			</Section>
 		</Layout>
 	)
