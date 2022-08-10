cad3a5fc4ea3669f1a521120b0aaf4fb9d09d597
Aksel Nordvig Ladegaard <an@acto.dk>
2022-08-11 00:39:10 +0200

    Theming & nesting in Sections
    Finally full width control!

--- src/templates/blogPost.js
+++ src/templates/blogPost.js
@@ -1,37 +1,37 @@
-import React         from 'react'
-import Layout        from '../components/layout/Layout'
-import {MDXRenderer} from 'gatsby-plugin-mdx'
+import React           from 'react'
+import Layout          from '../components/layout/Layout'
+import {MDXRenderer}   from 'gatsby-plugin-mdx'
 import {graphql, Link} from 'gatsby'
 import Section         from '../components/general/Section'
 import {MDXProvider}   from '@mdx-js/react'
+import {themes}        from "../abstracts/constants";
 
-const shortcodes = {Link} // Provide common components here
+const shortcodes = {Link, Section} // Provide common components here
 // take a look at the various remark plugins for transforming MDX.
 export default function BlogPost({data: {mdx}}) {
     return (
-        <Layout>
-            <Section>
-                <MDXProvider components={shortcodes}>
-                    <MDXRenderer>{mdx.body}</MDXRenderer>
-                </MDXProvider>
-            </Section>
+        <Layout theme={themes[mdx.frontmatter.theme]}>
+            <MDXProvider components={shortcodes}>
+                <MDXRenderer>{mdx.body}</MDXRenderer>
+            </MDXProvider>
         </Layout>
     )
 }
 export const pageQuery = graphql`
-	query BlogPostQuery($id: String) {
-		mdx(id: { eq: $id }) {
-			id
-			body
-			frontmatter {
-				dateCreated
+    query BlogPostQuery($id: String) {
+        mdx(id: { eq: $id }) {
+            id
+            body
+            frontmatter {
+                dateCreated
                 tags
                 type
                 misc
                 title
+                theme
                 description
                 dateUpdated
-			}
-		}
-	}
+            }
+        }
+    }
 `
