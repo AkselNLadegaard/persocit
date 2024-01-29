8eb209b0856429845aa1e93873a700eea132061b
Aksel N. Ladegaard <aksel@nordvig.eu>
2024-01-29 02:07:33 +0100

    Wrong type of comment-
    I should fix vscode, intellij blues..

--- src/abstracts/globalStyles.js
+++ src/abstracts/globalStyles.js
@@ -25,14 +25,14 @@ export const globalStyles = css`
 		min-height: calc(100vh - var(--navHeight) - var(--navOffset) - 8rem);
 	}
 
-	## total hack
+	// total hack
 	img {
 		box-shadow: none !important;
 		objectFit: contain !important;
 	}
-	## real solution will be to drop this silly fadein or fix aspect ratios or use another implementation for markdown files, but it is just so convenient....
-	## There also "must" be an official solution for this. Portrait images simply cannot just be this borked. 
-	## Also why doesn't gatsby-remark-images support all sharp options? I want to use the "fit" option to fix this issue, but it is not supported...
+	// real solution will be to drop this silly fadein or fix aspect ratios or use another implementation for markdown files, but it is just so convenient....
+	//  There also "must" be an official solution for this. Portrait images simply cannot just be this borked. 
+	// Also why doesn't gatsby-remark-images support all sharp options? I want to use the "fit" option to fix this issue, but it is not supported...
 
 	${basePseudoSelectorStyles};
 	${typographyStyles};
